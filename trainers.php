b560dcd915038e2c7636558900ac30b93266bd36
Ayushkatiyar92 <137542463+Ayushkatiyar92@users.noreply.github.com>
2024-07-07 21:35:04 +0530

    Add files via upload

---
+++ trainers.php
@@ -0,0 +1,14 @@
+<?php
+$Fname= $_POST['Fname']
+$Mname= $_POST['Mname']
+$Lname= $_POST['Lname']
+$DOB= $_POST['DOB']
+$number= $_POST['number']
+$country= $_POST['country']
+$pincode= $_POST['pincode']
+$address= $_POST['address']
+
+$state= $_POST['state']
+$countryCode= $_POST['countryCode']
+
+?>
